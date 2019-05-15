71074c476fbfaa7d64fd19a4806fdaa0affd2673
Gonzalo Escandarani <gonziesc@gmail.com>
2019-05-15 00:10:23 -0300

    adding niblement library for neuronal networks

--- index.py
+++ index.py
@@ -1,1 +1,24 @@
-print("This line will be printed.")
+import sys
+
+from nimblenet.activation_functions import sigmoid_function
+from nimblenet.cost_functions import cross_entropy_cost
+from nimblenet.learning_algorithms import RMSprop
+from nimblenet.data_structures import Instance
+from nimblenet.neuralnet import NeuralNet
+dataset        = [
+    Instance( [0,0], [0] ), Instance( [1,0], [1] ), Instance( [0,1], [1] ), Instance( [1,1], [0] )
+]
+settings       = {
+    "n_inputs" : 2,
+    "layers"   : [  (2, sigmoid_function), (1, sigmoid_function) ]
+}
+network        = NeuralNet( settings )
+training_set   = dataset
+test_set       = dataset
+cost_function  = cross_entropy_cost
+RMSprop(
+        network,           # the network to train
+        training_set,      # specify the training set
+        test_set,          # specify the test set
+        cost_function,     # specify the cost function to calculate error
+    )
