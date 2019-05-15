0442f90a40d31730b7b222f119a07a45e15fb370
Gonzalo Escandarani <gonziesc@gmail.com>
2019-05-15 00:35:40 -0300

    adding csv mapper

---
+++ csv-mapper.js
@@ -0,0 +1,33 @@
+const fs = require('fs'); 
+const parse = require('csv-parse');
+
+const csvData=[];
+const empresas=[];
+const rubros=[];
+
+fs.createReadStream('./reclamos-ingresados.csv')
+    .pipe(parse({delimiter: ';'}))
+    .on('data', function(csvrow) {
+        const obj =  {
+            empresa: csvrow[2],
+            rubro: csvrow[6],
+            estado: csvrow[11] || 'Pendiente',
+        };   
+        csvData.push(obj);
+    })
+    .on('end',function() {
+        csvData.forEach(elem => {
+            if (!empresas.find(empresa => empresa == elem.empresa)) {
+                empresas.push(elem.empresa);
+            }
+            if (!rubros.find(rubro => rubro == elem.rubro)) {
+                rubros.push(elem.rubro);
+            }
+        });
+      const dataset = csvData.map(elem => (
+          {input: [empresas.findIndex(e => e === elem.empresa), rubros.findIndex(e => e === elem.rubro)],
+           output: [elem.estado === 'Pendiente' ? 1 : 0]
+        }))
+       console.log(empresas[56]);
+      // console.log(require('util').inspect(dataset, { maxArrayLength: null }));
+    });
